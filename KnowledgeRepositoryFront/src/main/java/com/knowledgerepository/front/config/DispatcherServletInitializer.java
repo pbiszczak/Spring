44d2fe7d7e6393cc48fdb9e362a7785ef4f1ab48
Pawel Biszczak <pawel.biszczak@gmail.com>
2018-03-16 00:29:43 +0100

    First Commit

---
+++ KnowledgeRepositoryFront/src/main/java/com/knowledgerepository/front/config/DispatcherServletInitializer.java
@@ -0,0 +1,23 @@
+package com.knowledgerepository.front.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class DispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+
+        return new Class[]{ Configuraton.class };
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+
+        return new String[] { "/" };
+    }
+}
---
+++ KnowledgeRepositoryFront/src/main/java/com/knowledgerepository/front/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,8 @@
+package com.knowledgerepository.front.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer{
+
+
+}
